3bc5069895cf52007aedffeaaa8b609ad9cd3a49
MINASSAHIDDIN <minassahiddin@gmail.com>
2025-02-05 23:06:01 +0700

    this.value merujuk pada file html

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ const p1button = document.querySelector('#p1-button');
 const p2button = document.querySelector('#p2-button');
 const p1display = document.querySelector('#p1-display');
 const p2display = document.querySelector('#p2-display');
+const resetButton = document.querySelector('#riset')
+const winpointsOption = document.querySelector('#winpoin');
 
 let p1Score = 0;
 let p2Score = 0;
 let winpoints = 5;
 let isGameOver = false;
 // membuat deklrasi 
-
+function riset(){
+  isGameOver = false;
+  p1Score = 0;
+  p2Score = 0;
+  p1display.textContent = 0;
+  p2display.textContent = 0;
+}
 p1button.addEventListener('click', () => {
   if (!isGameOver) {
     p1Score += 1;
@@ -29,4 +37,13 @@ p2button.addEventListener('click', () => {
     }
   }
   p2display.textContent = p2Score;
+})
+
+resetButton.addEventListener('click', riset);
+
+winpointsOption.addEventListener('change', function(){
+  winpoints = parseInt(this.value);
+  // value merujuk pada value di html
+  riset();
+  // utnuk bisa dirubah
 })
